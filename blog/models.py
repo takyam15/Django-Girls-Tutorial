8a07617bb43dcb651818c17c296ac784a4f21d35
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-21 15:13:49 +0900

    Created the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,25 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-
-# Create your models here.
 
 class Author(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    title = models.CharField('title', max_length=200)
+    slug = models.SlugField('slug')
+    text = models.TextField('text')
+    created_date = models.DateTimeField('created_date', auto_now_add=True)
+    published_date = models.DateTimeField('published_date', blank=True, null=True)
+    updated_date = models.DateTimeField('updated_date', auto_now=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
